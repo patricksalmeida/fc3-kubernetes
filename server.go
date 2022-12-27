7f8cee02bea668e5687140657a4288245c6f9b60
Patrick Almeida <patricksalmeida@gmail.com>
2022-12-27 20:30:21 -0300

    feat: add mount and inject file into pod with configmap

--- server.go
+++ server.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
 	http.HandleFunc("/", Hello)
+	http.HandleFunc("/configmap", ConfigMap)
 	http.ListenAndServe(":8000", nil)
 }
 
@@ -16,4 +19,14 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	age := os.Getenv("AGE")
 	
 	fmt.Fprintf(w, "Hello, I'm %s, I'm %s old.", name, age)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile("myfamily/family.txt")
+
+	if err != nil {
+		log.Fatalf("Error reading file: ", err)
+	}
+
+	fmt.Fprintf(w, "My family: %s", string(data))
 }
